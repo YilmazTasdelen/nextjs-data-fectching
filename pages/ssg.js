796526d95d75532aeae2ad572a5c85661169f6f2
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-07-09 17:16:47 +0300

    .

---
+++ pages/ssg.js
@@ -0,0 +1,26 @@
+// static site generation
+import { YOUR_API_URL } from '../lib/api';
+
+export default function StaticSiteGeneration({ data }) {
+  return (
+    <>
+      {data.map((e) => (
+        <h2 key={e.id}>{e.name}</h2>
+      ))}
+    </>
+  );
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries.
+export async function getStaticProps() {
+  const res = await fetch(YOUR_API_URL);
+  const data = await res.json();
+
+  return {
+    props: {
+      data, // will be passed to the page component as props
+    },
+  };
+}
